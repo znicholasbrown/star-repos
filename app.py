4a36f4ea5f43ec8cd8331cb26732d8b7aba18e61
nicholas <znicholasbrown@gmail.com>
2020-09-16 14:47:46 -0400

    Add storage to flow

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import prefect
 from prefect import task, Flow, Parameter
 from prefect.schedules import clocks, Schedule
+from prefect.environments.storage import GitHub
 
 
 @task
@@ -13,6 +14,12 @@ schedule = Schedule(clocks=[clock])
 with Flow("Star GitHub Repositories", schedule=schedule) as flow:
     hello_world()
 
+
+flow.storage = GitHub(
+    repo="znicholasbrown/star-repos",
+    path="app.py",
+    secrets=["GITHUB_AUTH_TOKEN"],  # Change this to your own GitHub auth token secret
+)
 
 flow.register(project_name="SOME PROJECT")
 # flow.run()
